e24a9c4483c7c81edbf98c4cf810e2553484dea6
dsoni <dsoni@roblox.com>
2025-02-22 23:51:59 -0800

    persisting image of scanned QR code

--- lib/screens/qr_scanner_screen.dart
+++ lib/screens/qr_scanner_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'ble_unlock_screen.dart';
 import '/widgets/app_bar_with_nav.dart';
 
@@ -37,65 +38,73 @@ class _QRScannerScreenState extends State<QRScannerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // QR Scanner View inside a Column to prevent button pushing down
-            Container(
-              width: 300,
-              height: 300,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: scannedCode != null
-                        ? Colors.green.withOpacity(0.2)
-                        : Colors.blueAccent.withOpacity(0.5),
-                    blurRadius: 15,
-                    spreadRadius: 3,
-                  ),
-                ],
-                border: Border.all(color: Colors.lightBlue, width: 4),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: MobileScanner(
-                controller: scannerController,
-                onDetect: _onQRScanned,
-              ),
-            ),
-            const SizedBox(height: 20), // Add space between scanner and button
-            // Unlock Button
-            ElevatedButton(
-              onPressed: scannedCode != null
-                  ? () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                BLEUnlockScreen(scannedCode!)),
-                      );
-                    }
-                  : null, // Disabled if QR not scanned
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    scannedCode != null ? Colors.lightBlue : Colors.grey,
-                elevation: scannedCode != null ? 8 : 2,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 50, vertical: 18),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // QR Scanner View
+          Center(
+            child: RepaintBoundary(
+              key: _qrKey,
+              child: Container(
+                width: 300,
+                height: 300,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: scannedCode != null
+                          ? const Color.fromARGB(255, 234, 238, 234)
+                              .withOpacity(0.2)
+                          : Colors.blueAccent.withOpacity(0.1),
+                      blurRadius: 15,
+                      spreadRadius: 3,
+                    ),
+                  ],
+                  border: Border.all(color: Colors.lightBlue, width: 4),
+                  borderRadius: BorderRadius.circular(25),
                 ),
-              ),
-              child: const Text(
-                'Unlock Now',
-                style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
+                child: scannedCode != null
+                    ? QrImageView(
+                        data: scannedCode!,
+                        version: QrVersions.auto,
+                        size: 250.0,
+                      )
+                    : MobileScanner(
+                        controller: scannerController,
+                        onDetect: _onQRScanned,
+                      ),
               ),
             ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20),
+          // Unlock Button
+          ElevatedButton(
+            onPressed: scannedCode != null
+                ? () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => BLEUnlockScreen(scannedCode!)),
+                    );
+                  }
+                : null, // Disabled if QR not scanned
+            style: ElevatedButton.styleFrom(
+              backgroundColor:
+                  scannedCode != null ? Colors.lightBlue : Colors.grey,
+              elevation: scannedCode != null ? 8 : 2,
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 18),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+            child: Text(
+              'Unlock Now',
+              style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
       ),
     );
   }
